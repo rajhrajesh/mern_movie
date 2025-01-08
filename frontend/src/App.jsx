fe4c0cdfd57cca30b224932916b565716ff1f4a6
Rajesh P <penurajeshitsd@gmail.com>
2025-01-08 17:50:52 +0000

    footer components added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import Login from './pages/Login'
 import Home from './pages/home/Home'
+import Footer from './components/Footer'
 
 function App() {
-  return (
+  return <>
     <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
     </Routes>
-  )
+    <Footer/>
+    </>
 }
 
 export default App
