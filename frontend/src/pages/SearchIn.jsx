9ac900a541fba0285dc37b626cac07c9040c7a9e
Rajesh P <160295019+rajhrajesh@users.noreply.github.com>
2025-01-23 23:11:57 +0530

    Update SearchIn.jsx

--- frontend/src/pages/SearchIn.jsx
+++ frontend/src/pages/SearchIn.jsx
@@ -25,7 +25,7 @@ function SearchIn() {
     const handleSearch = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await axios.get(`/api/v1/search/${activeTab}/${searchTerm}`);
+			const res = await axios.get(`https://mern-movie-netflix-junior.onrender.com/api/v1/search/${activeTab}/${searchTerm}`);
 			setResults(res.data.content);
 		} catch (error) {
 			if (error.response.status === 404) {
